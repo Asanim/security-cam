c75984e7079434a0a2c35060dceaae6cc80a6bba
Sam <sam.taylor@prmeng.com.au>
2023-11-21 20:22:23 +1000

    refactored s3 uploader into a class

--- com.example.StreamManagerS3Upload/main.py
+++ com.example.StreamManagerS3Upload/main.py
@@ -1,12 +1,8 @@
-"""
-Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-"""
-
-
+import os
 import asyncio
 import logging
 import time
+import sys
 
 from stream_manager import (
     ExportDefinition,
@@ -25,112 +21,127 @@ from stream_manager import (
 )
 from stream_manager.util import Util
 
-# This example creates a local stream named "SomeStream", and a status stream named "SomeStatusStream.
-# It adds 1 S3 Export task into the "SomeStream" stream and then stream manager automatically exports
-# the data to a customer-created S3 bucket named "SomeBucket".
-# This example runs until the customer-created file at URL "SomeURL" has been uploaded to the S3 bucket.
+class S3Uploader:
+    def __init__(self, directory_path, stream_name, status_stream_name, bucket_name):
+        self.directory_path = directory_path
+        self.stream_name = stream_name
+        self.status_stream_name = status_stream_name
+        self.bucket_name = bucket_name
+        self.client = StreamManagerClient()
+        self.logger = logging.getLogger()
 
-
-def main(logger):
-    try:
-        stream_name = "SomeStream"
-        status_stream_name = "SomeStatusStreamName"
-        bucket_name = "video-bucket-517597205684"
-        key_name = "Zephyrus-G17"
-        file_url = "file:/home/sam/Downloads/angel-vox-Single.mp4"
-        client = StreamManagerClient()
-
-        # Try deleting the status stream (if it exists) so that we have a fresh start
+    def initialize_streams(self):
         try:
-            client.delete_message_stream(stream_name=status_stream_name)
-        except ResourceNotFoundException:
-            pass
+            # Try deleting the status stream (if it exists) so that we have a fresh start
+            try:
+                self.client.delete_message_stream(stream_name=self.status_stream_name)
+            except ResourceNotFoundException:
+                pass
 
-        # Try deleting the stream (if it exists) so that we have a fresh start
-        try:
-            client.delete_message_stream(stream_name=stream_name)
-        except ResourceNotFoundException:
-            pass
+            # Try deleting the stream (if it exists) so that we have a fresh start
+            try:
+                self.client.delete_message_stream(stream_name=self.stream_name)
+            except ResourceNotFoundException:
+                pass
 
-        exports = ExportDefinition(
-            s3_task_executor=[
-                S3ExportTaskExecutorConfig(
-                    identifier="S3TaskExecutor" + stream_name,  # Required
-                    # Optional. Add an export status stream to add statuses for all S3 upload tasks.
-                    status_config=StatusConfig(
-                        status_level=StatusLevel.INFO,  # Default is INFO level statuses.
-                        # Status Stream should be created before specifying in S3 Export Config.
-                        status_stream_name=status_stream_name,
-                    ),
-                )
-            ]
-        )
-
-        # Create the Status Stream.
-        client.create_message_stream(
-            MessageStreamDefinition(name=status_stream_name, strategy_on_full=StrategyOnFull.OverwriteOldestData)
-        )
-
-        # Create the message stream with the S3 Export definition.
-        client.create_message_stream(
-            MessageStreamDefinition(
-                name=stream_name, strategy_on_full=StrategyOnFull.OverwriteOldestData, export_definition=exports
+            exports = ExportDefinition(
+                s3_task_executor=[
+                    S3ExportTaskExecutorConfig(
+                        identifier="S3TaskExecutor" + self.stream_name,
+                        status_config=StatusConfig(
+                            status_level=StatusLevel.INFO,
+                            status_stream_name=self.status_stream_name,
+                        ),
+                    )
+                ]
             )
-        )
 
-        # Append a S3 Task definition and print the sequence number
-        s3_export_task_definition = S3ExportTaskDefinition(input_url=file_url, bucket=bucket_name, key=key_name)
-        logger.info(
-            "Successfully appended S3 Task Definition to stream with sequence number %d",
-            client.append_message(stream_name, Util.validate_and_serialize_to_json_bytes(s3_export_task_definition)),
-        )
+            # Create the Status Stream.
+            self.client.create_message_stream(
+                MessageStreamDefinition(name=self.status_stream_name, strategy_on_full=StrategyOnFull.OverwriteOldestData)
+            )
 
-        # Read the statuses from the export status stream
+            # Create the message stream with the S3 Export definition.
+            self.client.create_message_stream(
+                MessageStreamDefinition(
+                    name=self.stream_name, strategy_on_full=StrategyOnFull.OverwriteOldestData, export_definition=exports
+                )
+            )
+        except Exception as e:
+            self.logger.exception(f"Exception during stream initialization: {e}")
+
+    def upload_files(self):
+        try:
+            # Iterate through files in the specified directory
+            for filename in os.listdir(self.directory_path):
+                file_path = os.path.join(self.directory_path, filename)
+
+                # Append a S3 Task definition and print the sequence number
+                s3_export_task_definition = S3ExportTaskDefinition(
+                    input_url=f"file:{file_path}", bucket=self.bucket_name, key=filename
+                )
+                sequence_number = self.client.append_message(
+                    self.stream_name, Util.validate_and_serialize_to_json_bytes(s3_export_task_definition)
+                )
+                self.logger.info(
+                    f"Successfully appended S3 Task Definition to stream with sequence number {sequence_number}"
+                )
+
+            # Monitor the status of file uploads
+            self.monitor_status()
+
+        except Exception as e:
+            self.logger.exception(f"Exception during file upload: {e}")
+
+    def monitor_status(self):
         stop_checking = False
         next_seq = 0
         while not stop_checking:
             try:
-                messages_list = client.read_messages(
-                    status_stream_name,
+                messages_list = self.client.read_messages(
+                    self.status_stream_name,
                     ReadMessagesOptions(
                         desired_start_sequence_number=next_seq, min_message_count=1, read_timeout_millis=1000
                     ),
                 )
                 for message in messages_list:
-                    # Deserialize the status message first.
                     status_message = Util.deserialize_json_bytes_to_obj(message.payload, StatusMessage)
 
-                    # Check the status of the status message. If the status is "Success",
-                    # the file was successfully uploaded to S3.
-                    # If the status was either "Failure" or "Cancelled", the server was unable to upload the file to S3.
-                    # We will print the message for why the upload to S3 failed from the status message.
-                    # If the status was "InProgress", the status indicates that the server has started uploading
-                    # the S3 task.
                     if status_message.status == Status.Success:
-                        logger.info("Successfully uploaded file at path " + file_url + " to S3.")
-                        stop_checking = True
+                        self.logger.info(f"Successfully uploaded file with sequence number {message.sequence_number} to S3.")
                     elif status_message.status == Status.InProgress:
-                        logger.info("File upload is in Progress.")
+                        self.logger.info("File upload is in Progress.")
                         next_seq = message.sequence_number + 1
-                    elif status_message.status == Status.Failure or status_message.status == Status.Canceled:
-                        logger.info(
-                            "Unable to upload file at path " + file_url + " to S3. Message: " + status_message.message
+                    elif status_message.status in [Status.Failure, Status.Canceled]:
+                        self.logger.info(
+                            f"Unable to upload file with sequence number {message.sequence_number} to S3. Message: {status_message.message}"
                         )
-                        stop_checking = True
+
                 if not stop_checking:
                     time.sleep(5)
-            except StreamManagerException:
-                logger.exception("Exception while running")
+            except StreamManagerException as e:
+                self.logger.exception(f"Exception while running: {e}")
                 time.sleep(5)
-    except asyncio.TimeoutError:
-        logger.exception("Timed out while executing")
-    except Exception:
-        logger.exception("Exception while running")
-    finally:
-        if client:
-            client.close()
+            except Exception as e:
+                self.logger.exception(f"Unhandled exception during status monitoring: {e}")
 
+    def cleanup(self):
+        try:
+            if self.client:
+                self.client.close()
+        except Exception as e:
+            self.logger.exception(f"Exception during cleanup: {e}")
 
-logging.basicConfig(level=logging.INFO)
-# Start up this sample code
-main(logger=logging.getLogger())
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    directory_path = args[0]
+
+    logging.basicConfig(level=logging.INFO)
+    stream_name = "SomeStream"
+    status_stream_name = "SomeStatusStreamName"
+    bucket_name = "video-bucket-517597205684"
+
+    uploader = S3Uploader(directory_path, stream_name, status_stream_name, bucket_name)
+    uploader.initialize_streams()
+    uploader.upload_files()
+    uploader.cleanup()
