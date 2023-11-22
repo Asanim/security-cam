5ba2385bf4294f21a55e514fe3b72f3b190075ca
Asanim <samjt775@gmail.com>
2023-11-22 19:17:00 +0800

    feat: Add conversion script for yolov7 to trt

---
+++ scripts/convert-yolov7.sh
@@ -0,0 +1,5 @@
+cd $HOME/security-cam/yolov7
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-tiny.pt
+python export.py --weights ./yolov7-tiny.pt --grid --end2end --simplify --topk-all 100 --iou-thres 0.65 --conf-thres 0.35 --img-size 640 640
+git clone https://github.com/Linaom1214/tensorrt-python.git
+python ./tensorrt-python/export.py -o yolov7-tiny.onnx -e yolov7-tiny-nms.trt -p fp16
--- scripts/init-build-machine.sh
+++ scripts/init-build-machine.sh
@@ -1,4 +1,14 @@
-sudo apt install -y libnvinfer-dev libnvinfer-plugin-dev libnvonnxparsers-dev libnvparsers-dev tensorrt tensorrt-libs python3-libnvinfer-dev libjsoncpp-dev cuda-cudart-dev-11-4 cuda-cupti-dev-11-4 cuda-driver-dev-11-4 cuda-libraries-dev-11-4 cuda-nvml-dev-11-4 cuda-nvrtc-dev-11-4 libcusolver-dev-11-4
+# Install necessary libraries
+sudo apt update
+sudo apt install -y libnvinfer-dev libnvinfer-plugin-dev libnvonnxparsers-dev libnvparsers-dev tensorrt tensorrt-libs python3-libnvinfer-dev libjsoncpp-dev cuda-cudart-dev-11-4 cuda-cupti-dev-11-4 cuda-driver-dev-11-4 cuda-libraries-dev-11-4 cuda-nvml-dev-11-4 cuda-nvrtc-dev-11-4 libcusolver-dev-11-4 libpoco-dev
 
 # Download the SSD model.engine file
 wget -O ssd_mobilenet_v2_coco.engine https://github.com/jkjung-avt/tensorrt_demos/releases/download/v1.0/ssd_mobilenet_v2_coco.engine
+
+# Clone YOLOv7 repository
+git clone https://github.com/WongKinYiu/yolov7.git $HOME/security-cam/yolov7
+
+
+# Clone YOLOv8 repository
+git clone https://github.com/ultralytics/yolov8.git $HOME/security-cam/yolov8
+
